d481ab1c7363191abfc5c02ca94ca42edcb02ffb
Alper Çelik <alper.celikk1@outlook.com>
2021-01-23 20:35:42 +0300

    Add project files.

---
+++ MyDictionaryOdev/MyDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryOdev
+{
+    class MyDictionary<TKey,TValue>
+    {
+        private TKey[] keys;
+        private TValue[] values;
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKeys = keys;
+            TValue[] tempValues = values;
+
+            keys = new TKey[keys.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+
+            keys[keys.Length - 1] = key;
+
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+            }
+
+            values[values.Length - 1] = value;
+
+        }
+
+        public TValue[] value
+        {
+            get { return values; }
+        }
+
+        public TKey[] key
+        {
+            get { return keys; }
+        }
+    }
+}
---
+++ MyDictionaryOdev/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionaryOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> Test = new Dictionary<string, int>();
+            Test.Add("alper",32);
+
+            foreach (var t in Test)
+            {
+                MyDictionary<int, string> AdYas = new MyDictionary<int, string>();
+
+                AdYas.Add(32,"Alper");
+                AdYas.Add(5, "Rana");
+                AdYas.Add(2, "Elif");
+
+                for (int i = 0; i < AdYas.value.Length; i++)
+                {
+                    Console.WriteLine(AdYas.value[i] + " yaşı : " + AdYas.key[i]);
+                }
+            }
+        }
+    }
+}
