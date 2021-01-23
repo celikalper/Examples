246f79255024c3e70891ce9a3e9d9b42c39940e7
Alper Çelik <alper.celikk1@outlook.com>
2021-01-23 20:36:43 +0300

    bitirdim hocam

--- MyDictionaryOdev/Program.cs
+++ MyDictionaryOdev/Program.cs
@@ -7,22 +7,18 @@ namespace MyDictionaryOdev
     {
         static void Main(string[] args)
         {
-            Dictionary<string, int> Test = new Dictionary<string, int>();
-            Test.Add("alper",32);
+            
+             MyDictionary<int, string> AdYas = new MyDictionary<int, string>();
 
-            foreach (var t in Test)
-            {
-                MyDictionary<int, string> AdYas = new MyDictionary<int, string>();
+             AdYas.Add(32,"Alper");
+             AdYas.Add(5, "Rana");
+             AdYas.Add(2, "Elif");
 
-                AdYas.Add(32,"Alper");
-                AdYas.Add(5, "Rana");
-                AdYas.Add(2, "Elif");
-
-                for (int i = 0; i < AdYas.value.Length; i++)
-                {
-                    Console.WriteLine(AdYas.value[i] + " yaşı : " + AdYas.key[i]);
-                }
-            }
+             for (int i = 0; i < AdYas.value.Length; i++)
+             {
+                 Console.WriteLine(AdYas.value[i] + " yaşı : " + AdYas.key[i]);
+             }
+            
         }
     }
 }
